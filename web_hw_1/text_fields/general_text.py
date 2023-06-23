4c6c09baa3f83e2d9155fbd685d7b656625a920e
Yaroslav <yaroslawchovnyuk@gmail.com>
2023-06-23 21:03:22 +0300

    first version

---
+++ web_hw_1/text_fields/general_text.py
@@ -0,0 +1,9 @@
+
+class GeneralText:
+
+    start_message = \
+    '\n{:^40}\n{:^40}\n'.format('---HELLO---', '-'*40)+\
+    '|{:^38}|\n'.format("I'M WILLY ASSISTANT v0.9.*")+\
+    '|{:^38}|\n{:^40}\n'.format("NICE TO MEET YOU!", '-'*40)
+    continue_input_message = 'Press enter to continue.\n>>> '
+    wrong_input_message = '\nHmm.. Somesing wrong. Try again.\n'
